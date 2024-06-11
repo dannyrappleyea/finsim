168154497aa1ab2acae74e7e187eb448b24def29
Danny Rappleyea <dannyrappleyea@users.noreply.github.com>
2024-06-11 19:54:26 -0400

    use better compound interest formula

--- finsim.py
+++ finsim.py
@@ -14,16 +14,23 @@ import numpy as np
 #ts.resample("ME").sum()
 #st.area_chart(ts.resample("ME").sum())
 
-dates = pd.date_range(start='2024-01-01', end='2024-12-31', name='Date')
+initial_balance = 100
+rate = 0.05
+dates = pd.date_range(start='2024-01-01', end='2024-12-31', name='date')
 #df = pd.DataFrame(np.random.randint(0, 5, size=len(dates)), index=dates, columns = ['deposit'])
 df = pd.DataFrame(index=dates)
-df["deposit"] = 1
+df["numdays"] = (df.index - df.index[0]) / np.timedelta64(1, 'D')
+# df["balance"] = initial_balance * 
 
-df["rate"] = 0.05/365.0
+# df.at['2024-01-01', 'deposit'] = 100
+# df["deposit"] = 0
+
+# df["rate"] = 0.05/365.0
 # df["interest"] = df["deposit"] * df["rate"]
-df['total'] = (df['deposit'] * df['rate'].shift().add(1).cumprod().fillna(1)).cumsum() # From https://stackoverflow.com/questions/57620050/calculate-compound-interest-in-pandas
-st.dataframe(df)
+# df['total'] = (df['deposit'] * df['rate'].shift().add(1).cumprod().fillna(1)).cumsum() # From https://stackoverflow.com/questions/57620050/calculate-compound-interest-in-pandas
+df['total'] = initial_balance * ((1+(rate/365)) ** (365 * (df["numdays"]/365)))
+st.dataframe(df, use_container_width=True)
 st.area_chart(df, y="total")
-df1 = df.resample("M").last()
+df1 = df.resample("ME").last()
 st.dataframe(df1)
 st.area_chart(df1)
