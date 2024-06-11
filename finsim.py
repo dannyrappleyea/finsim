377ac45325a7c3d46787de45bd38d8c48a1321bd
Danny Rappleyea <dannyrappleyea@users.noreply.github.com>
2024-06-11 18:56:06 -0400

    add basic compounding interest

--- finsim.py
+++ finsim.py
@@ -3,8 +3,27 @@ import pandas as pd
 import numpy as np
 
 
+# dates = pd.date_range(start='2024-01-01', end='2024-12-31', name='Date')
+# df = pd.DataFrame(np.random.randint(0, 10, size=len(dates)), index=dates, columns = ['value'])
+# df1 = df.resample("M").sum()
+# st.dataframe(df1)
+# st.area_chart(df1)
+# st.area_chart(df)
 
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+#ts = pd.Series(np.random.randint(0, 10, size=len(dates)), index=dates)
+#ts.resample("ME").sum()
+#st.area_chart(ts.resample("ME").sum())
 
-st.dataframe(chart_data)
-st.bar_chart(chart_data)
+dates = pd.date_range(start='2024-01-01', end='2024-12-31', name='Date')
+#df = pd.DataFrame(np.random.randint(0, 5, size=len(dates)), index=dates, columns = ['deposit'])
+df = pd.DataFrame(index=dates)
+df["deposit"] = 1
+
+df["rate"] = 0.05/365.0
+# df["interest"] = df["deposit"] * df["rate"]
+df['total'] = (df['deposit'] * df['rate'].shift().add(1).cumprod().fillna(1)).cumsum()
+st.dataframe(df)
+st.area_chart(df, y="total")
+df1 = df.resample("M").last()
+st.dataframe(df1)
+st.area_chart(df1)
